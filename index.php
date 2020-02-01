1667ea194ec800e3644c1c4cde38085f60993a52
Maxim Koshman <maksimkoshman090@gmail.com>
2020-02-01 21:58:15 +0300

    create output ip addresses - base AJAX request

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AJAX</title>
+</head>
+<body>
+    <input id="shop_ip" type="button" value="Показать мой IP">
+    <div id="myip"></div>
+
+    <script src="script.js"></script>
+</body>
+</html>
